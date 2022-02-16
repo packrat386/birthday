e10247061a22ec2f3481c91bc888562c416af76e
Aidan Coyle <packrat386@gmail.com>
2022-02-16 14:27:16 -0600

    Use time zones
    I live in chicago so we need to interpret WHEN_IS_MY_BIRTHDAY in
    Chicago time rather than in UTC. So the timestamp I'm working with
    becomes one without a specified timezone and I use a particular one to
    turn it into a moment in time that I can use to generate a range.
    
    But what if I'm somewhere else for my birthday?

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,9 @@
 require 'active_support/all'
 
+Time.zone = 'America/Chicago'
+
 class Birthday
-  WHEN_IS_MY_BIRTHDAY = Time.parse('2022-11-28T00:00:00Z')
+  WHEN_IS_MY_BIRTHDAY = Time.zone.parse('2022-11-28T00:00:00')
 
   def self.is_my_birthday?(me)
     Time.now >= WHEN_IS_MY_BIRTHDAY &&
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 RSpec.describe Birthday do
   let(:me) { double('me') }
+  let(:us_central_tz) { ActiveSupport::TimeZone['America/Chicago'] }
 
   before { Timecop.freeze(now) }
   after { Timecop.return }
 
   context 'On some other day' do
-    let(:now) { Time.parse('2022-05-05T00:00:00Z') }
+    let(:now) { us_central_tz.parse('2022-05-05T00:00:00') }
 
     it 'is not my birthday' do
       expect(described_class.is_my_birthday?(me)).to be false
@@ -16,7 +17,7 @@ RSpec.describe Birthday do
 
   context 'On November 28th 2022' do
     context 'at midgnight' do
-      let(:now) { Time.parse('2022-11-28T00:00:00Z') }
+      let(:now) { us_central_tz.parse('2022-11-28T00:00:00') }
 
       it 'is my birthday' do
         expect(described_class.is_my_birthday?(me)).to be true
@@ -24,7 +25,15 @@ RSpec.describe Birthday do
     end
 
     context 'at 8 AM' do
-      let(:now) { Time.parse('2022-11-28T08:00:00Z') }
+      let(:now) { us_central_tz.parse('2022-11-28T08:00:00') }
+
+      it 'is my birthday' do
+        expect(described_class.is_my_birthday?(me)).to be true
+      end
+    end
+
+    context 'at 11 PM' do
+      let(:now) { us_central_tz.parse('2022-11-28T23:00:00') }
 
       it 'is my birthday' do
         expect(described_class.is_my_birthday?(me)).to be true
