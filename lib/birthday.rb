cd24e0b8266affe5e6dfc39bc5023f11824c2569
Aidan Coyle <packrat386@gmail.com>
2022-02-16 14:01:29 -0600

    Simplest time based example
    We set a Time for WHEN_IS_MY_BIRTHDAY. If it is that time right now,
    then it is my birthday.
    
    But what will this say if I wake up at 8 AM and check if it's my
    birthday?

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,9 @@
 require 'active_support/all'
 
 class Birthday
+  WHEN_IS_MY_BIRTHDAY = Time.parse('2022-11-28T00:00:00Z')
+
   def self.is_my_birthday?(me)
-    false
+    return Time.now == WHEN_IS_MY_BIRTHDAY
   end
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -3,7 +3,22 @@ require 'spec_helper'
 RSpec.describe Birthday do
   let(:me) { double('me') }
 
-  it 'knows if it is my birthday' do
-    expect(described_class.is_my_birthday?(me)).to be false
+  before { Timecop.freeze(now) }
+  after { Timecop.return }
+
+  context 'On some other day' do
+    let(:now) { Time.parse('2022-05-05T00:00:00Z') }
+
+    it 'is not my birthday' do
+      expect(described_class.is_my_birthday?(me)).to be false
+    end
+  end
+
+  context 'On November 28th 2022' do
+    let(:now) { Time.parse('2022-11-28T00:00:00Z') }
+
+    it 'is my birthday' do
+      expect(described_class.is_my_birthday?(me)).to be true
+    end
   end
 end
