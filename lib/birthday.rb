3a087a684dbd11e5c918a55114acfde1802358d1
Aidan Coyle <packrat386@gmail.com>
2022-02-16 14:39:41 -0600

    Support my local timezone
    Because my birthday is where I am (I have an ego, so what :P) and I
    can move around, I need to be able to have this code check whether the
    current time falls within the range that is my birthday in my local
    timezone. What this means is that now I store WHEN_IS_MY_BIRTHDAY as a
    string and then parse it with the passed tz. Then use the instant in
    time denoted by parsing that time string in the passed tz to construct
    an interval and check that the current local time is within it.
    
    This code is for a pretty trivial example, but it's already getting
    kinda hairy. I need to remember whenever I interpret
    WHEN_IS_MY_BIRTHDAY to use the timezone associated with the person
    who's birthday it is, and I have to be careful about all the usual
    errors with things falling inside of ranges.
    
    Is there a better way?

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,10 +3,14 @@ require 'active_support/all'
 Time.zone = 'America/Chicago'
 
 class Birthday
-  WHEN_IS_MY_BIRTHDAY = Time.zone.parse('2022-11-28T00:00:00')
+  WHEN_IS_MY_BIRTHDAY = '2022-11-28T00:00:00'
 
   def self.is_my_birthday?(me)
-    Time.now >= WHEN_IS_MY_BIRTHDAY &&
-      Time.now < WHEN_IS_MY_BIRTHDAY + 1.day
+    Time.current >= local_birthday_begin(me.tz) &&
+      Time.current < local_birthday_begin(me.tz) + 1.day
+  end
+
+  def self.local_birthday_begin(tz)
+    tz.parse(WHEN_IS_MY_BIRTHDAY)
   end
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Birthday do
-  let(:me) { double('me') }
+  let(:me) { double('me', tz: my_tz) }
   let(:us_central_tz) { ActiveSupport::TimeZone['America/Chicago'] }
+  let(:london_tz) { ActiveSupport::TimeZone['Europe/London'] }
+  let(:my_tz) { us_central_tz }
 
   before { Timecop.freeze(now) }
   after { Timecop.return }
 
-  context 'On some other day' do
+  context 'on some other day' do
     let(:now) { us_central_tz.parse('2022-05-05T00:00:00') }
 
     it 'is not my birthday' do
@@ -15,28 +17,76 @@ RSpec.describe Birthday do
     end
   end
 
-  context 'On November 28th 2022' do
-    context 'at midgnight' do
-      let(:now) { us_central_tz.parse('2022-11-28T00:00:00') }
+  context 'on November 28th 2022' do
+    context 'I am in Chicago' do
+      let(:my_tz) { us_central_tz }
 
-      it 'is my birthday' do
-        expect(described_class.is_my_birthday?(me)).to be true
+      context 'at 5 AM in London' do
+        let(:now) { london_tz.parse('2022-11-28T05:00:00') }
+
+        it 'is not my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be false
+        end
+      end
+
+      context 'at midgnight in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T00:00:00') }
+
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
+      end
+
+      context 'at 8 AM in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T08:00:00') }
+
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
+      end
+
+      context 'at 11 PM in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T23:00:00') }
+
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
       end
     end
 
-    context 'at 8 AM' do
-      let(:now) { us_central_tz.parse('2022-11-28T08:00:00') }
+    context 'I am in London' do
+      let(:my_tz) { london_tz }
 
-      it 'is my birthday' do
-        expect(described_class.is_my_birthday?(me)).to be true
+      context 'at 5 AM in London' do
+        let(:now) { london_tz.parse('2022-11-28T05:00:00') }
+
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
       end
-    end
 
-    context 'at 11 PM' do
-      let(:now) { us_central_tz.parse('2022-11-28T23:00:00') }
+      context 'at midgnight in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T00:00:00') }
 
-      it 'is my birthday' do
-        expect(described_class.is_my_birthday?(me)).to be true
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
+      end
+
+      context 'at 8 AM in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T08:00:00') }
+
+        it 'is my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be true
+        end
+      end
+
+      context 'at 11 PM in Chicago' do
+        let(:now) { us_central_tz.parse('2022-11-28T23:00:00') }
+
+        it 'is not my birthday' do
+          expect(described_class.is_my_birthday?(me)).to be false
+        end
       end
     end
   end
