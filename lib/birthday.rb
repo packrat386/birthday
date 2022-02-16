a6edd3369c1a429c76bb9afa6014561ff2230384
Aidan Coyle <packrat386@gmail.com>
2022-02-16 14:48:23 -0600

    Use a Date instead of a Time
    My birthday is not a time, nor is it a time range. My birthday is a
    date. So the solution here is simple. Ask me what day it is where I
    am and then compare it to the date that I know is my birthday. This
    code is almost as simple as the most trivial possible code that we
    wrote using Time, but it handles _all_ of the edge cases that we had
    to add so much code for.
    
    Dates are not suitable for all things. Sometimes you really do need to
    denote a range in time (such as the opening hours of a store). However
    if what you want to model is something happening on a certain day, you
    will incur much pain by trying to use a range in time to represent the
    day. Just use a Date.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,14 +3,9 @@ require 'active_support/all'
 Time.zone = 'America/Chicago'
 
 class Birthday
-  WHEN_IS_MY_BIRTHDAY = '2022-11-28T00:00:00'
+  WHEN_IS_MY_BIRTHDAY = Date.new(2022, 11, 28)
 
   def self.is_my_birthday?(me)
-    Time.current >= local_birthday_begin(me.tz) &&
-      Time.current < local_birthday_begin(me.tz) + 1.day
-  end
-
-  def self.local_birthday_begin(tz)
-    tz.parse(WHEN_IS_MY_BIRTHDAY)
+    me.tz.now.to_date == WHEN_IS_MY_BIRTHDAY
   end
 end
